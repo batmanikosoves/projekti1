3cb6e11f950a7298024305286c3a8dd055d755c7
caliii1231 <ensar.thaci@hotmail.com>
2024-05-15 17:39:51 +0200

    projekt1

--- js/1.js
+++ js/1.js
@@ -83,4 +83,28 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+let slideIndex = 0;
 
+function showSlides() {
+    const slides = document.querySelectorAll('.slide');
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    } else if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${100 * (index - slideIndex)}%)`;
+    });
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlides();
+}
+
+function prevSlide() {
+    slideIndex--;
+    showSlides();
+}
+
+showSlides();
