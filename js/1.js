48340bd8737db7d75762dbae62dc1099da6ff4fb
Tekanjozi <168005372+batmanikosoves@users.noreply.github.com>
2024-05-27 17:46:37 +0200

    projekt1

--- js/1.js
+++ js/1.js
@@ -111,3 +111,19 @@ document.addEventListener("DOMContentLoaded", function() {
       emailInput.value = ""; // Clear the input field after submission
   });
 });
+
+
+function search_animal() {
+  let input = document.getElementById('searchbar').value
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName('animals');
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    }
+    else {
+      x[i].style.display = "list-item";
+    }
+  }
+}
